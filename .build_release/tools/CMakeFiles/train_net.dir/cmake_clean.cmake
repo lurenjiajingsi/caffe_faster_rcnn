ed1fa19a6618933a3ff5da2cb51eabb4cd6a3775
lurenjiajingsi <976725198@qq.com>
2020-12-23 10:51:52 +0800

    caffe faster_rcnn rpn

---
+++ .build_release/tools/CMakeFiles/train_net.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train_net.dir/train_net.cpp.o"
+  "train_net"
+  "train_net.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/train_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
